d2d509c5c5dae189d4d161780e019edddf005e07
eionidi <cat81.06@gmail.com>
2017-08-16 22:59:29 +0200

    find string with letters

--- main.rb
+++ main.rb
@@ -1,3 +1,9 @@
 require "./random_string_array.rb"
 
-puts random_string_array
+random_string_array.each do |e|
+  should_print = false
+  should_print = true if ['k', 'a', 't', 'e'].all? { |l| e.include? l }
+  if should_print == true
+    puts e
+  end
+end

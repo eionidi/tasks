295a5a6d96ba6b7c27bcbbda9bb1f2d11948dfe9
eionidi <cat81.06@gmail.com>
2017-09-11 22:35:11 +0200

    fix

--- main.rb
+++ main.rb
@@ -1,5 +1,3 @@
-require "./random_string_array.rb"
+require './random_string_array.rb'
 
-puts random_string_array.select { |s| ['k', 'a', 't', 'e'].all? { |l| s.include? l } }
-
-
+puts(random_string_array.select { |s| %w(k a t e).all? { |l| s.include? l } })
--- random_string_array.rb
+++ random_string_array.rb
@@ -1,9 +1,8 @@
-SYMS = ['a'..'z'].map{ |range| range.to_a }.flatten
+SYMS = ['a'..'z'].map(&:to_a).flatten
 def random_string(length)
   length.times.inject('') { |string, _letter| string << SYMS[rand(SYMS.size - 1)] }
 end
 
 def random_string_array
-  rand(12..45).times.map { random_string(rand(15..45)) }
+  Array.new(rand(12..45)) { random_string(rand(15..45)) }
 end
-
